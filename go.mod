06b2b0b7b01b6441cbce854a63241d2ce467c326
xdd <991465510@qq.com>
2021-07-09 18:33:30 +0800

    格式化代码文件夹和文件，并设置go.mod

---
+++ go.mod
@@ -0,0 +1,6 @@
+module example
+
+go 1.13
+
+require din v0.0.0
+replace din=>./din
--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
+	"din/din"
 	"fmt"
-	"html"
-	"log"
 	"net/http"
 )
 
-func FooHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
-type Engine struct {
-}
-
-func (e *Engine) ServeHTTP(rep http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/foo":
-		fmt.Fprintf(rep, "%q", req.URL.Path)
-		break
-	case "/test":
-		fmt.Fprintf(rep, "%q", "444555")
-		break
-	default:
-		fmt.Fprintf(rep, "%q", "默认数据")
-	}
-}
-
 func main() {
-	//http.HandleFunc("/foo", FooHandle)
-	eng := new(Engine)
-	log.Fatal(http.ListenAndServe(":8080",
-		eng))
+	d:=din.New()
+	d.Get("/foo", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer,"%q","this is foo")
+	})
+
 }
